093f8d204ce596b694d5d4d7c22e3e4d394f9017
Vincenzo Gargano <vincenzo.gargano@edu.unito.it>
2020-05-27 15:41:56 +0200

    Initialized test branch for test

---
+++ Desktop/Appunti_Università/Progetto_Database/data_model1_table_create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE public.utente (
+    email  NOT NULL,
+    PRIMARY KEY (email)
+);
+
+
+CREATE TABLE public.voto (
+);
+
+
+CREATE TABLE public.contenuto (
+    nome  NOT NULL,
+    PRIMARY KEY (nome)
+);
+
---
+++ Desktop/Appunti_Università/Progetto_Database/data_model1_table_drop.sql
@@ -0,0 +1,3 @@
+DROP TABLE public.utente CASCADE;
+DROP TABLE public.voto CASCADE;
+DROP TABLE public.contenuto CASCADE;
